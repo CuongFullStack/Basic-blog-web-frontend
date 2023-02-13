e4fa07652fdec027e1fe2ad9d17d7dc7370f1715
Vu Cuong <vuvietcuong1351998@gmail.com>
2023-02-13 16:52:09 +0700

    create PostPage

--- src/Post.js
+++ src/Post.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { format } from "date-fns";
+import { Link } from "react-router-dom";
 
-const Post = ({ title, summary, cover, content, createdAt, author }) => {
+const Post = ({ _id, title, summary, cover, content, createdAt, author }) => {
   return (
     <div className="post">
       <div className="image">
-        <img src={"http://localhost:4000/" + cover} alt="" />
+        <Link to={`/post/${_id}`}>
+          <img src={"http://localhost:4000/" + cover} alt="" />
+        </Link>
       </div>
       <div className="texts">
-        <h2>{title}</h2>
+        <Link to={`post/${_id}`}>
+          <h2>{title}</h2>
+        </Link>
         <p className="info">
           <span className="author">{author.username}</span>
           <time>{format(new Date(createdAt), "MMM d, yyyy HH:mm")}</time>
