a1b97e96106216d5d8c1bfa26a0645cc6b4291b2
Vu Cuong <vuvietcuong1351998@gmail.com>
2023-02-14 16:55:35 +0700

    css, create component EditPost

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,35 +1,7 @@
 import React, { useState } from "react";
-import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { Navigate } from "react-router-dom";
-
-const modules = {
-  toolbar: [
-    [{ header: [1, 2, false] }],
-    ["bold", "italic", "underline", "strike", "blockquote"],
-    [
-      { list: "ordered" },
-      { list: "bullet" },
-      { indent: "-1" },
-      { indent: "+1" },
-    ],
-    ["link", "image"],
-    ["clean"],
-  ],
-};
-const formats = [
-  "header",
-  "bold",
-  "italic",
-  "underline",
-  "strike",
-  "blockquote",
-  "list",
-  "bullet",
-  "indent",
-  "link",
-  "image",
-];
+import Editor from "../Editor";
 
 const CreatePost = () => {
   const [title, setTitle] = useState("");
@@ -79,12 +51,8 @@ const CreatePost = () => {
         // value={files}
         onChange={(ev) => setFiles(ev.target.files)}
       />
-      <ReactQuill
-        value={content}
-        module={modules}
-        formats={formats}
-        onChange={(newValue) => setContent(newValue)}
-      />
+      <Editor value={content} onChange={setContent} />
+
       <button style={{ marginTop: "5px" }}>Create post</button>
     </form>
   );
